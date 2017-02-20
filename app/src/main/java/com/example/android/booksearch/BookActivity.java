d3f53310d03376ad610ccba804aced3d3483bdff
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-02-20 13:01:13 -0600

    check network connection functionality was added

--- app/src/main/java/com/example/android/booksearch/BookActivity.java
+++ app/src/main/java/com/example/android/booksearch/BookActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,18 +16,9 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.android.booksearch.QueryUtils.LOG_TAG;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -35,15 +27,17 @@ public class BookActivity extends AppCompatActivity {
     private static final String BOOKS_REQUEST_URL =
             " https://www.googleapis.com/books/v1/volumes?q=subject+";
 
-    // ListView bookListView ; caso de ponerlo como en UDACITY GITHUB
-
     ImageButton searchButton;
     EditText inputSearch ;
     TextView userInfo ;
 
+    BookAdapter mAdapter ; //Global mAdapter that modifies on each bookListUpdating
+
     public static final String LOG_TAG = BookActivity.class.getName();
 
+    /* Constant with the query that indicates maxResults*/
     public static final String RESULTS = "&maxResults=15" ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +47,16 @@ public class BookActivity extends AppCompatActivity {
         searchButton =(ImageButton) findViewById(R.id.searchButton);
         inputSearch = (EditText) findViewById(R.id.inputSearch) ;
         userInfo = (TextView) findViewById(R.id.info_tv);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView  bookListView = (ListView) findViewById(R.id.list) ;
+
+        // Create a new adapter that takes an empty list of books as input
+        mAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        bookListView.setAdapter(mAdapter);
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +74,11 @@ public class BookActivity extends AppCompatActivity {
         });
     }
 
+    /*
+    InternetConnection is an extended asyncTask to perform an HTTPRequest to Google Books API server in background
+    @String It passes a string with the established url for performing the query
+    @List<Book>  doInBackground method will return a List<Book> with the parsed data from server
+     */
     private class InternetConnection extends AsyncTask <String, Void, List<Book> > {
 
         @Override
@@ -99,23 +108,29 @@ public class BookActivity extends AppCompatActivity {
                 return;
             }
 
-            // Find a reference to the {@link ListView} in the layout
-            ListView bookListView = (ListView) findViewById(R.id.list);
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
 
-            // Create a new {@link ArrayAdapter} of earthquakes
-            final BookAdapter adapter = new BookAdapter( BookActivity.this , books );
-
-            // Set the adapter on the {@link ListView}
-            // so the list can be populated in the user interface
-            bookListView.setAdapter(adapter);
+            // If there is a valid list of {@link Books}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (books != null && !books.isEmpty()) {
+                mAdapter.addAll(books);
+            }
         }
     }
 
+    /*
+    This method formats the search query of the editText. In addtion it  appends maxAmount of results
+     */
     private String formatSearch (String query){
 
         return query.replace(" ", "+") + RESULTS ;
     }
 
+    /*
+    This method uses a ConnectivityManager instance for checking the state of the Internet connection
+    It will show a toast in case no Internet connection is found
+     */
     private void checkInternetConenction() {
 
         ConnectivityManager cm =(ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
